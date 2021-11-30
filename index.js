d12eb5ab4a8a453d3dff59d902b6726a23ee5477
Anudeep <anudeep.hanuman@gmail.com>
2021-12-01 00:15:26 +0530

    added table for displaying registrations and added new JSON display route for registrations

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ env.config();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+app.set("view engine","ejs");
 app.use(registerRoute);
 app.use(viewRoute);
 
--- routes/view.js
+++ routes/view.js
@@ -5,6 +5,17 @@ var Participant=require('../models/participant');
 router.get("/viewAllRegistrations",async(req,res)=>{
     try{
         let participants=await Participant.find();
+        // participants.push({count:participants.length});
+        res.render("viewAll",{participants:participants});
+        // res.status(200).send(participants);
+    }catch(err){
+        res.status(400).send({message:err});
+    }
+});
+
+router.get("/viewAllRegistrationsJSON",async(req,res)=>{
+    try{
+        let participants=await Participant.find();
         participants.push({count:participants.length});
         res.status(200).send(participants);
     }catch(err){
